4522b6a95937e520ef4e1c87f54943b4e7e0e306
akifbayram <56325239+akifbayram@users.noreply.github.com>
2023-11-05 12:20:51 -0500

    Add country-state-city package, Add state dropdown selector

--- react-app/src/components/RegisterField.js
+++ react-app/src/components/RegisterField.js
@@ -1,57 +1,58 @@
 import React, { useState } from "react";
 import { Navigate } from "react-router-dom";
-import { Container, Row, Col, Form, Button, FormCheck, Spinner, ButtonGroup, ToggleButton } from "react-bootstrap";
+import { Container, Row, Col, Form, Button, FormCheck, Dropdown, Spinner, ButtonGroup, ToggleButton } from "react-bootstrap";
 import { useAuth } from "../hooks/useAuth";
 import { useSignup } from "../hooks/useSignup";
+import { State } from "country-state-city";
 
 // Main registration form containing form logic and fields
 const RegisterField = ({ setAlertMessage, setAlertType }) => {
   const [userRole, setUserRole] = useState("client");
   const [wantsLocal, setWantsLocal] = useState(false);
+  const states = State.getStatesOfCountry("US");
   const [formData, setFormData] = useState({
     first_name: "",
     last_name: "",
     email: "",
     username: "",
     password: "",
-    // street_address: "",
-    // city: "",
-    // state: "",
-    // zip_code: "",
-    // country: "",
+    street_address: "",
+    city: "",
+    state: "",
+    zip_code: "",
   });
 
-    const {user} = useAuth();
-    const {signup, isLoading, errorMessage} = useSignup();
-  
-    const roles = [
-      { name: "Coach", value: "coach" },
-      { name: "Client", value: "client" },
-    ];
-  
-    const handleInputChange = (e) => {
-      const { id, value } = e.target;
-      setFormData({ ...formData, [id]: value });
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      console.log("Submitting form data:", formData);
-      console.log("User role:", userRole);
+  const { user } = useAuth();
+  const { signup, isLoading, errorMessage } = useSignup();
 
-      await signup(formData, userRole);
+  const roles = [
+    { name: "Coach", value: "coach" },
+    { name: "Client", value: "client" },
+  ];
 
-      if(errorMessage){
-        setAlertMessage(errorMessage);
-        setAlertType('danger')
-      }
-    };
-  
-    return (
-      <Container className="mt-3 justify-content-center">
-        <Row className="bg-light text-dark rounded p-4">
-          <Col>
-            <Form onSubmit={handleSubmit}>
+  const handleInputChange = (e) => {
+    const { id, value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Submitting form data:", formData);
+    console.log("User role:", userRole);
+
+    await signup(formData, userRole);
+
+    if (errorMessage) {
+      setAlertMessage(errorMessage);
+      setAlertType("danger");
+    }
+  };
+
+  return (
+    <Container className="mt-3 justify-content-center">
+      <Row className="bg-light text-dark rounded p-4">
+        <Col>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className=" d-grid" controlId="role">
               <Form.Label>Select Role</Form.Label>
               <ButtonGroup className="mb-3">
@@ -77,28 +78,28 @@ const RegisterField = ({ setAlertMessage, setAlertType }) => {
             <Row className="mb-3">
               <Col>
                 <Form.Label>First Name</Form.Label>
-                <Form.Control id="first_name" type="text" value={formData.first_name} onChange={handleInputChange} />
+                <Form.Control required id="first_name" type="text" value={formData.first_name} onChange={handleInputChange} />
               </Col>
               <Col>
                 <Form.Label>Last Name</Form.Label>
-                <Form.Control id="last_name" type="text" value={formData.last_name} onChange={handleInputChange} />
+                <Form.Control required id="last_name" type="text" value={formData.last_name} onChange={handleInputChange} />
               </Col>
             </Row>
 
             {/* Remaining Fields */}
             <Form.Group className="mb-3" controlId="email">
               <Form.Label>Email</Form.Label>
-              <Form.Control type="email" value={formData.email} onChange={handleInputChange} />
+              <Form.Control required type="email" value={formData.email} onChange={handleInputChange} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="username">
               <Form.Label>Username</Form.Label>
-              <Form.Control type="text" value={formData.username} onChange={handleInputChange} />
+              <Form.Control required type="text" value={formData.username} onChange={handleInputChange} />
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="password">
               <Form.Label>Password</Form.Label>
-              <Form.Control type="password" value={formData.password} onChange={handleInputChange} />
+              <Form.Control required type="password" value={formData.password} onChange={handleInputChange} />
             </Form.Group>
 
             <Form.Group className="mb-3">
@@ -115,51 +116,54 @@ const RegisterField = ({ setAlertMessage, setAlertType }) => {
               <>
                 <Form.Group className="mb-3" controlId="street_address">
                   <Form.Label>Street Address</Form.Label>
-                  <Form.Control type="text" value={formData.street_address} onChange={handleInputChange} />
+                  <Form.Control type="text" value={formData.street_address} onChange={handleInputChange} required={wantsLocal} />
                 </Form.Group>
                 <Row className="mb-3">
                   <Col>
                     <Form.Group controlId="city">
                       <Form.Label>City</Form.Label>
-                      <Form.Control type="text" value={formData.city} onChange={handleInputChange} />
+                      <Form.Control type="text" value={formData.city} onChange={handleInputChange} required={wantsLocal} />
                     </Form.Group>
                   </Col>
                   <Col>
                     <Form.Group controlId="state">
                       <Form.Label>State</Form.Label>
-                      <Form.Control type="text" value={formData.state} onChange={handleInputChange} />
+                      <Form.Select value={formData.state} onChange={handleInputChange} required={wantsLocal}>
+                        <option value="">Select State</option>
+                        {states.map((state) => (
+                          <option key={state.isoCode} value={state.isoCode}>
+                            {state.name}
+                          </option>
+                        ))}
+                      </Form.Select>
                     </Form.Group>
                   </Col>
                 </Row>
                 <Form.Group className="mb-3" controlId="zip_code">
                   <Form.Label>Zip Code</Form.Label>
-                  <Form.Control type="text" value={formData.zip_code} onChange={handleInputChange} required={wantsLocal} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="country">
-                  <Form.Label>Country</Form.Label>
-                  <Form.Control type="text" value={formData.country} onChange={handleInputChange} />
+                  <Form.Control
+                    type="text"
+                    value={formData.zip_code}
+                    onChange={handleInputChange}
+                    required={wantsLocal}
+                    pattern="^\d{5}(-\d{4})?$"
+                    isInvalid={!!formData.zip_code && !/^\d{5}(-\d{4})?$/.test(formData.zip_code)}
+                  />
                 </Form.Group>
               </>
             )}
-  
-              <div className="d-grid">
-                <Button size="lg" variant="primary" type="submit" disabled={isLoading}>
-                  {isLoading ? <Spinner animation="border" size="sm" /> : "Submit"}
-                </Button>
-              </div>
-            </Form>
-          </Col>
-        </Row>
-        {user && <Navigate to='/onboard'/>}
-      </Container>
-    );
-  };
+
+            <div className="d-grid">
+              <Button size="lg" variant="primary" type="submit" disabled={isLoading}>
+                {isLoading ? <Spinner animation="border" size="sm" /> : "Submit"}
+              </Button>
+            </div>
+          </Form>
+        </Col>
+      </Row>
+      {user && <Navigate to="/onboard" />}
+    </Container>
+  );
+};
 
 export default RegisterField;
-
-
-
-
-
-
-
--- react-app/src/hooks/useSignup.js
+++ react-app/src/hooks/useSignup.js
@@ -5,53 +5,56 @@ import { useState } from "react";
 const url = "http://localhost:3500/";
 
 export const useSignup = () => {
-    const [isLoading, setIsLoading] = useState(null);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const {dispatch} = useAuth();
-    const {setItem} = useLocalStorage();
+  const [isLoading, setIsLoading] = useState(null);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const { dispatch } = useAuth();
+  const { setItem } = useLocalStorage();
 
-    const signup = async (formData, userRole) => {
-        try{
-            setIsLoading(true);
-            setErrorMessage(null);
+  const signup = async (formData, userRole) => {
+    try {
+      setIsLoading(true);
+      setErrorMessage(null);
 
-            const response = await fetch(`${url}insert_user/`, {
-                method: "POST",
-                headers: {
-                  // Moved data to body instead of headers
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify({
-                  first_name: formData.first_name,
-                  last_name: formData.last_name,
-                  username: formData.username,
-                  email: formData.email,
-                  password: formData.password,
-                  role: userRole,
-                }),
-                credentials: "include", // Include credentials with the request
-              });
-              const data = await response.json();
+      const response = await fetch(`${url}insert_user/`, {
+        method: "POST",
+        headers: {
+          // Moved data to body instead of headers
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          first_name: formData.first_name,
+          last_name: formData.last_name,
+          username: formData.username,
+          email: formData.email,
+          password: formData.password,
+          role: userRole,
+          street_address: formData.street_address,
+          city: formData.city,
+          state: formData.state,
+          zip_code: formData.zip_code,
+        }),
+        credentials: "include", // Include credentials with the request
+      });
+      const data = await response.json();
 
-            if (response.ok) {
-                const username = formData.username;
-                //save the user to local storage
-                setItem('user', JSON.stringify({username}));
+      if (response.ok) {
+        const username = formData.username;
+        //save the user to local storage
+        setItem("user", JSON.stringify({ username }));
 
-                //update the auth context
-                dispatch({type: "LOGIN", payload : username});
+        //update the auth context
+        dispatch({ type: "LOGIN", payload: username });
 
-                setIsLoading(false);
+        setIsLoading(false);
+      } else {
+        setErrorMessage(data.message);
+        console.error("Error occurred during Signup:", response.status);
+      }
+    } catch (err) {
+      setErrorMessage("The backend is down. Please try again later.");
+      console.log(err);
+    }
+  };
 
-            } else {
-                setErrorMessage(data.message);
-                console.error("Error occurred during Signup:", response.status);
-            }
-        } catch(err){
-            setErrorMessage("The backend is down. Please try again later.")
-            console.log(err);
-        }
-    };
-
-    return {signup, isLoading, errorMessage};
-}
+  return { signup, isLoading, errorMessage };
+};
