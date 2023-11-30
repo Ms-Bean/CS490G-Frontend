f427c20b7f6d465ed66e11b0a13525c1e6bd8acb
Oscar Velazquez <oscarvelazquez@oscars-mbp-2.mynetworksettings.com>
2023-11-30 00:14:48 -0500

    updated workout plan info

--- react-app/src/components/WorkoutPlan/WorkoutPlanInfo.js
+++ react-app/src/components/WorkoutPlan/WorkoutPlanInfo.js
@@ -2,7 +2,45 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function WorkoutPlanInfo({workoutPlanName}) {
+const tempExercise = [
+    {
+      "exercise": "Squat",
+      "time": "10 AM",
+      "day": "Tuesday",
+      "sets": "5x5",
+      "weight": "145lb"
+    },
+    {
+      "exercise": "ОНР",
+      "time": "2 PM",
+      "day": "Tuesday",
+      "sets": "5x5",
+      "weight": "85lb"
+    },
+    {
+      "exercise": "Deadlift",
+      "time": "2 PM",
+      "day": "Wednesday",
+      "sets": "5x5",
+      "weight": "185lb"
+    },
+    {
+      "exercise": "BB Row",
+      "time": "2 PM",
+      "day": "Thursday",
+      "sets": "5x5",
+      "weight": "125lb"
+    }
+  ]
+
+const tempGoals = [
+    'Build Muscle',
+    'Improve Endurance',,
+    'Reduce Body Fat Percentage',
+    'Improve Posture',
+];
+
+function WorkoutPlanInfo({workoutPlanName, exercises, goals}) {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
@@ -21,15 +59,37 @@ function WorkoutPlanInfo({workoutPlanName}) {
         keyboard={false}
         centered
         >
-        <Modal.Header closeButton>
-            <Modal.Title>{workoutPlanName}</Modal.Title>
+        <Modal.Header className='text-center' closeButton>
+            <Modal.Title className='w-100'>
+            {workoutPlanName}
+            </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            Workout Plan Info goes here
+            <div className='w-75 mx-auto'>
+                <small>Goals:
+                    {tempGoals.map((goal, index) => (
+                        <span className='ms-1' key={`goal-${index}`}>{goal},</span>
+                    ))}
+                </small>
+            </div>
+            <div className='mt-3'>
+                <div className='border-bottom '>
+                    <h5 className='ms-3'>Workout Plan</h5>
+                </div>
+                <ul class="list-group">
+                    {tempExercise.map((e, index) => (
+                         <li class="list-group-item border-0 border-bottom d-flex">
+                            <div className='me-auto'>
+                                {e.exercise}
+                            </div>
+                            <div>
+                                {e.time} {e.day} {e.sets} {e.weight}
+                            </div>
+                         </li>
+                    ))}
+                </ul>
+            </div>
         </Modal.Body>
-        <Modal.Footer>
-            <Button className='w-100' variant="dark">Edit</Button>
-        </Modal.Footer>
         </Modal>
     </>
   );
