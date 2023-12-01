9a4ec3d4422459fbb031c94e9d28a9e5517db70a
Hasan Ozgur <62520352+hberkozgur@users.noreply.github.com>
2023-12-01 17:42:15 -0500

    get message works. only post message left to do.

--- react-app/src/views/MessagePage.js
+++ react-app/src/views/MessagePage.js
@@ -1,15 +1,18 @@
+// MessagePage.js
+
 import React, { useState, useEffect } from "react";
 import { Container, Card, Row, Col, Spinner, Alert } from "react-bootstrap";
-import CarouselComp from "../components/Messaging";
+import CarouselComp from "../components/CarouselComp";
 import { useAuth } from "../hooks/useAuth";
 
 const MessagePage = () => {
   const { user } = useAuth();
-  const [userList, setUserList] = useState([]);
   const [selectedUserId, setSelectedUserId] = useState(null);
   const [messages, setMessages] = useState([]);
+  const [selectedUserMessages, setSelectedUserMessages] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,8 +24,8 @@ const MessagePage = () => {
 
         if (response.ok) {
           const responseData = await response.json();
-          console.log("Messages List:", responseData.coachesList);
-          setMessages(responseData.coachesList);
+          const user_id_List = responseData.user_id_List;
+          setMessages(user_id_List);
         } else {
           console.error("Error fetching messages list:", response.statusText);
           setError("Error fetching messages");
@@ -35,38 +38,94 @@ const MessagePage = () => {
       }
     };
 
-    // Fetch data when the component mounts
     fetchData();
   }, [user]);
 
-  const handleUserClick = (userId) => {
-    setSelectedUserId(userId);
-    // Additional logic if needed
+  const handleUserClick = async (selectedUserId) => {
+    const UserId = user.user_id;
+    setSelectedUserId(selectedUserId);
+    console.log("Selected User ID:", selectedUserId);
+    await fetchUserMessages(UserId, selectedUserId, currentPage);
+  };
+
+  const fetchUserMessages = async (userId, otherUserId, page_num) => {
+    try {
+      const response = await fetch(`http://localhost:3500/messages?user_id=${userId}&other_user_id=${otherUserId}&page_size=10&page_num=${page_num}`, {
+        method: "GET",
+        credentials: "include",
+      });
+
+      if (response.ok) {
+        const responseData = await response.json();
+        setSelectedUserMessages(responseData.messages);
+      } else {
+        console.error("Error fetching user messages:", response.statusText);
+        setError("Error fetching user messages");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      setError("Error");
+    }
+  };
+
+  const renderUserList = (role) => {
+    const users = messages.filter((user) => user.role === role);
+    return users.map((user) => (
+      <li key={user.id.id} onClick={() => handleUserClick(user.id.id)}>
+        {user.id.name}
+      </li>
+    ));
+  };
+
+  const renderClientList = () => {
+    const clients = messages.filter((user) => user.role === "client");
+
+    if (clients.length === 0) {
+      return <p>No clients to message.</p>;
+    }
+
+    return (
+      <Card className="text-center bg-light">
+        <Card.Body>
+          <Card.Title>
+            <h1>Client List</h1>
+          </Card.Title>
+          <ul>{renderUserList("client")}</ul>
+        </Card.Body>
+      </Card>
+    );
+  };
+
+  const renderCoachList = () => {
+    const coaches = messages.filter((user) => user.role === "coach");
+
+    if (coaches.length === 0) {
+      return <p>No coaches to message.</p>;
+    }
+
+    return (
+      <Card className="text-center bg-light">
+        <Card.Body>
+          <Card.Title>
+            <h1>Coach List</h1>
+          </Card.Title>
+          <ul>{renderUserList("coach")}</ul>
+        </Card.Body>
+      </Card>
+    );
   };
 
   return (
     <Container className="mt-5">
       <Row>
-        {/* User List on the left */}
+        {/* Coaches List and Client List stacked on top of each other on the left */}
         <Col md={3}>
-          <Card className="text-center bg-light">
-            <Card.Body>
-              <Card.Title>
-                <h1>User List</h1>
-              </Card.Title>
-              <ul>
-                {userList.map((user) => (
-                  <li key={user.user_id} onClick={() => handleUserClick(user.user_id)}>
-                    {user.name}
-                  </li>
-                ))}
-              </ul>
-            </Card.Body>
-          </Card>
+          {renderCoachList()}
+          {renderClientList()}
         </Col>
 
         {/* Messaging History on the right */}
-        <Col md={9}>
+        <Col md={6}>
           <Card className="text-center bg-light">
             <Card.Body>
               <Card.Title>
@@ -79,7 +138,20 @@ const MessagePage = () => {
                   {selectedUserId ? (
                     <>
                       <p>Selected User: {selectedUserId}</p>
-                      <CarouselComp messages={messages} />
+                      {selectedUserMessages.length > 0 ? (
+                        <div>
+                          <h2>Messages:</h2>
+                          <ul>
+                            {selectedUserMessages.map((message) => (
+                              <li key={message.message_id}>
+                                {message.content} - {message.created}
+                              </li>
+                            ))}
+                          </ul>
+                        </div>
+                      ) : (
+                        <p>No messages available.</p>
+                      )}
                     </>
                   ) : (
                     <p>Select a user to view messages.</p>
