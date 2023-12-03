260b84c47113cf71a6d4957b8bce3d61ee8554d8
Spencer Clarke <spence12310@icloud.com>
2023-12-03 08:38:06 -0500

    Finished client dashboard

--- react-app/src/views/Dashboard.js
+++ react-app/src/views/Dashboard.js
@@ -1,21 +1,12 @@
 import React from "react";
 import { Container, Card, Button } from "react-bootstrap";
+import ClientDashboard from "../components/Dashboard";
 
 const Dashboard = () => {
   return (
-    <Container className="mt-5">
-      <Card className="text-center bg-light">
-        <Card.Body>
-          <Card.Title>
-            <h1>Dashboard</h1>
-          </Card.Title>
-          <Card.Text>This will be your dashboard.</Card.Text>
-          <Button variant="primary" href="#">
-            Let's Go!
-          </Button>
-        </Card.Body>
-      </Card>
-    </Container>
+    <>
+      <ClientDashboard/>
+    </>
   );
 };
 
