e0e16cdd8ab00446680add1214876f53a472e792
akifbayram <56325239+akifbayram@users.noreply.github.com>
2023-12-02 21:41:06 -0500

    Add react-youtube package, Add embed to exercise

--- react-app/src/components/Exercises/ExerciseViewModal.js
+++ react-app/src/components/Exercises/ExerciseViewModal.js
@@ -1,10 +1,33 @@
 import React from "react";
+import { Container } from "react-bootstrap";
+import YouTube from "react-youtube";
+import "./../../css/ExerciseBank.css";
 
 const ViewExerciseModal = ({ selectedExercise }) => {
   const extractLabels = (items) => items.map((item) => item.label).join(", ");
 
+  const getYouTubeVideoId = (url) => {
+    if (!url) return null;
+  
+    const regExp = /https:\/\/www\.youtube\.com\/watch\?v=(\d+)/;
+    const match = url.match(regExp);
+    return match ? match[1] : null;
+  };
+  
+  const videoId = getYouTubeVideoId(selectedExercise.video_link);
+
+  const options = {
+    playerVars: {
+      autoplay: 0,
+      controls: 0,
+    },
+  };
+
   return (
     <div>
+      <Container className="youtube-container">
+      {videoId && <YouTube videoId={videoId} opts={options} />}
+      </Container>
       <p>
         <strong>Name:</strong> {selectedExercise.name}
       </p>
@@ -15,7 +38,7 @@ const ViewExerciseModal = ({ selectedExercise }) => {
         <strong>Difficulty:</strong> {selectedExercise.difficulty}
       </p>
       <p>
-        <strong>Goals:</strong> {selectedExercise.goal_name}
+        <strong>Fitness Goals:</strong> {selectedExercise.goal_name}
       </p>
       <p>
         <strong>Muscle Groups: </strong>
@@ -24,12 +47,6 @@ const ViewExerciseModal = ({ selectedExercise }) => {
       <p>
         <strong>Equipment: </strong>
         {selectedExercise.equipment_items?.length > 0 ? extractLabels(selectedExercise.equipment_items) : "None"}
-      </p>
-      <p>
-        <strong>Video Link:</strong>{" "}
-        <a href={selectedExercise.video_link} target="_blank" rel="noopener noreferrer">
-          View Video
-        </a>
       </p>
     </div>
   );
