35ed0e6de849bbbec8f0b4eae10cf61903bb8b5b
Oscar Velazquez <oscarvelazquez@oscars-mbp-2.resource.campus.njit.edu>
2023-12-07 15:06:04 -0500

    merge workout navbar style

--- react-app/src/components/WorkoutPlan/WorkoutNavbar.js
+++ react-app/src/components/WorkoutPlan/WorkoutNavbar.js
@@ -26,20 +26,33 @@ const WorkoutNavbar = ({onSort, onSearch, sortKey, onToggleSortDirection, sortDi
             <div className="container">
                 <div className="navbar-brand">
                     Workout Plan
-                    <NewWorkoutPlan handleUploadSuccessChange={handleUploadSuccessChange}/>
                 </div>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <NewWorkoutPlan handleUploadSuccessChange={handleUploadSuccessChange}/>
+                <button 
+                className="navbar-toggler" 
+                type="button" 
+                data-bs-toggle="collapse" 
+                data-bs-target="#navbarSupportedContent" 
+                aria-controls="navbarSupportedContent" 
+                aria-expanded="false" 
+                aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <form className="d-flex ms-auto" role="search">
-                        <input onChange={handleSearchChange} className="form-control" type="search" placeholder="Search By Workout" aria-label="Search"/>
-                        <button className="btn btn-secondary ms-2" type="submit">
+                        <input onChange={handleSearchChange} className="form-control ms-auto" type="search" placeholder="Search By name..." aria-label="Search"/>
+                        <span className="navbar-text-link align-self-center" variant="secondary" type="submit">
+                        <FaSearch />
+                        </span>
+                        {/* <button className="btn btn-secondary ms-2" type="submit">
                             <FaSearch />
-                        </button>
-                        <Dropdown variant="secondary" className="ms-2" onSelect={handleSortOptionClick}>
-                            <Dropdown.Toggle variant="secondary" id="dropdown-basic-button">
-                                Sort By
+                        </button> */}
+                        <span className="navbar-text-link align-self-center" style={{ cursor: "pointer" }}>
+                          Filter By ▾
+                        </span>
+                        <Dropdown className=" navbar-text-link align-self-center ms-2" onSelect={handleSortOptionClick}>
+                            <Dropdown.Toggle as="span" id="dropdown-basic-button" style={{cursor: "pointer"}}>
+                              Sort By ▾
                             </Dropdown.Toggle>
                             <Dropdown.Menu>
                                 <Dropdown.Item eventKey="name">Name{getSortDirectionSymbol("name")}</Dropdown.Item>
