b38d599a9c5753aa27c4b15b532bb6fb406a0d3b
akifbayram <56325239+akifbayram@users.noreply.github.com>
2023-12-10 10:28:13 -0500

    Style login page

--- react-app/src/views/LoginPage.js
+++ react-app/src/views/LoginPage.js
@@ -4,26 +4,25 @@ import LoginForm from "../components/LoginForm";
 import { useAuth } from "../hooks/useAuth";
 
 const LoginPage = () => {
-
   return (
-    <Container className="mt-3 p-4 bg-primary text-white rounded w-50 mx-auto">
+    <Container className="my-5 p-4 bg-dark text-white rounded w-50 mx-auto">
       <Container className="">
         <h1 className="mt-2 mb-2 text-center">Sign In</h1>
         <div className="mt-3">
           <p className="mb-0  text-center">
-            Don't have an account?{" "}
+            Don't have an account? 
             <a href="/registration" className="text-white fw-bold">
-              Sign Up
+               Sign Up
             </a>
           </p>
-        </div>{" "}
+        </div>
       </Container>
       <Container className="mt-3 justify-content-center">
         <Row className="bg-light text-dark rounded p-4">
           <Col>
             <Row>
               <Col>
-                <LoginForm setUser={"hello"} />
+                <LoginForm />
               </Col>
             </Row>
           </Col>
