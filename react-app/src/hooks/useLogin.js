4f166d609c55ce8a146a4e1aef65bd3fac34785a
akifbayram <56325239+akifbayram@users.noreply.github.com>
2023-12-14 16:24:36 -0500

    Update useLogin hook to handle user data object

--- react-app/src/hooks/useLogin.js
+++ react-app/src/hooks/useLogin.js
@@ -4,41 +4,42 @@ import { useState } from "react";
 import { config } from "./../utils/config";
 
 export const useLogin = () => {
-    const [isLoading, setIsLoading] = useState(null);
-    const [errorMessage, setErrorMessage] = useState(null);
-    const {dispatch} = useAuth();
-    const {setItem} = useLocalStorage();
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const { dispatch } = useAuth();
+  const { setItem } = useLocalStorage();
 
-    const login = async (username, password) => {
-        try{
-            setIsLoading(true);
-            setErrorMessage(null);
+  const login = async (username, password) => {
+    setIsLoading(true);
+    setErrorMessage(null);
 
-            const response = await fetch(`${config.backendUrl}/login`, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({username, password}),
-                credentials: "include",
-            });
+    try {
+      const response = await fetch(`${config.backendUrl}/login`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ username, password }),
+        credentials: "include",
+      });
 
-            const data = await response.json();
+      const data = await response.json();
 
-            if (response.ok) {
-                setItem('user', JSON.stringify({username}));
+      if (response.ok) {
+        const user = data.user; 
+        setItem("user", JSON.stringify(user));
+        dispatch({ type: "LOGIN", payload: user });
+      } else {
+        setErrorMessage(data.message || "Login failed. Please try again.");
+        console.error("Error occurred during login:", response.status, data.message);
+      }
+    } catch (err) {
+      setErrorMessage("The backend is down or an error occurred. Please try again later.");
+      console.error("Login error:", err);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-                dispatch({type: "LOGIN", payload : {username}});
+  return { login, isLoading, errorMessage };
+};
 
-                setIsLoading(false);
-
-            } else {
-                setErrorMessage(data.message);
-                console.error("Error occurred during login:", response.status);
-            }
-        } catch(err){
-            setErrorMessage("The backend is down. Please try again later.")
-            console.log(err);
-        }
-    };
-
-    return {login, isLoading, errorMessage};
-}
+export default useLogin;
