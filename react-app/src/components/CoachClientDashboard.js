6ebe52e27cdc59e359958003e71edcf2afe96d8b
adrianaska0 <adrian.aska004@gmail.com>
2023-12-15 09:12:02 -0500

    coach can terminate client

--- react-app/src/components/CoachClientDashboard.js
+++ react-app/src/components/CoachClientDashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, ButtonGroup, Table, Container, Dropdown, Image, DropdownButton, Row, Col, Modal } from "react-bootstrap";
+import { Button, ButtonGroup, Table, Container, Dropdown, Image, DropdownButton, Row, Col, Modal, Alert } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import NewWorkoutPlan from "./WorkoutPlan/NewWorkoutPlan";
 import { config } from "./../utils/config";
@@ -11,9 +11,16 @@ const CoachClientDashboard = () => {
   const [uploadSuccess, setUploadSuccess] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
   const [show, setShow] = useState();
+  const [showTerm, setShowTerm] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [showAlert2, setShowAlert2] = useState(false);
+
+  const [termClient, setTermClient] = useState([]);
 
   const handleClose = () => setShow(false);
+  const handleCloseTerm = () => setShowTerm(false);
   const handleShow = () => setShow(true);
+  const handleShowTerm = () => setShowTerm(true);
 
   const handleAnalyticsClick = (clientId) => {
     navigate(`?client_id=${clientId}#statisticsView`);
@@ -26,6 +33,11 @@ const CoachClientDashboard = () => {
   const handleAssignClick = (clientId) => {
 
     navigate("../select_workout_plan?user_id=" + clientId);
+  }
+
+  const handleTermClient = (client) => {
+    setTermClient(client);
+    handleShowTerm();
   }
 
   useEffect(() => {
@@ -49,10 +61,34 @@ const CoachClientDashboard = () => {
         setIsLoading(false);
       }
     };
-
     fetch_coach_dashboard_info();
     setUploadSuccess(false);
   }, [uploadSuccess]);
+
+  const terminateClient = async () => {
+    try{
+      const response = await fetch(`${config.backendUrl}/terminate/${termClient.client_id}`, {
+        method: "DELETE", 
+        headers: {
+            "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
+      console.log(response);
+      if (!response.ok) {
+        setShowAlert2(true);
+        throw new Error("Network response was not ok");
+      }
+      else{
+        handleUploadSuccessChange()
+        setShowAlert(true);
+        handleCloseTerm();
+      }
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
 
   // useEffect(() => {
   //     //Fetch client profile information
@@ -107,6 +143,8 @@ const CoachClientDashboard = () => {
 
   return (
     <Container>
+      <Alert variant="success" show={showAlert}>You are no longer {termClient.first_name} {termClient.last_name}'s coach.</Alert>
+      <Alert variant="danger" show={showAlert2}>Failed to end training with {termClient.first_name} {termClient.last_name}.</Alert>
       <div className="d-flex justify-content-between align-items-center">
         <h4 className="mb-2">Your Clients</h4>
         <Button className="mb-2" onClick={handleShow}>
@@ -121,6 +159,20 @@ const CoachClientDashboard = () => {
           <AcceptDenyClients handleUploadSuccessChange={handleUploadSuccessChange}/>
         </Modal.Body>
         <Modal.Footer><Button className='w-100' onClick={handleClose} variant="dark">Done</Button></Modal.Footer>
+      </Modal>
+
+      <Modal size="md" show={showTerm} onHide={handleCloseTerm} backdrop="static" keyboard={false} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>End Training</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <center>
+          <p>Are you sure you want to end training with <b>{termClient.first_name} {termClient.last_name}</b>?</p>
+          <p>This will permanently delete all interactions between you and the client (messages, workout plans, etc.)</p>
+          <b>This action cannot be undone.</b>
+          </center>
+        </Modal.Body>
+        <Modal.Footer><Button className='w-100' onClick={()=> terminateClient()} variant="danger">End Training</Button></Modal.Footer>
       </Modal>
       <table className="table responsive">
         <thead>
@@ -172,7 +224,7 @@ const CoachClientDashboard = () => {
                     <Dropdown.Divider />
                     <Dropdown.Item onClick={() => handleAssignClick(client.client_id)}>Assign Workout Plan</Dropdown.Item>
                     <Dropdown.Divider />
-                    <Dropdown.Item variant="danger" onClick={() => navigate(`#`)}>
+                    <Dropdown.Item variant="danger" onClick={() => handleTermClient(client)}>
                       End Training
                     </Dropdown.Item>
                   </DropdownButton>
