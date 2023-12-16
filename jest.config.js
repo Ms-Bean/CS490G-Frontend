9519eadffb750b43cf8f27b3fada4859d3350c22
Gunraj Singh <103146222+Gunraj10@users.noreply.github.com>
2023-12-16 12:34:59 -0500

    testing, continued....

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-    "transform": {
-      '^.+\\.(js|jsx)?$': 'babel-jest',
-    }
-  };
-
-  
-
-  
+  testEnvironment: "jsdom",
+  setupFiles: ["<rootDir>/react-app/src/setupTests.js"],
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest',
+  }
+};
