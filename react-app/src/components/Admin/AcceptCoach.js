895da1b2b7b035d772c485c8ba3ed655eb9caa75
akifbayram <56325239+akifbayram@users.noreply.github.com>
2023-12-18 01:24:08 -0500

    Refactor AcceptCoach and RejectCoach components

--- react-app/src/components/Admin/AcceptCoach.js
+++ react-app/src/components/Admin/AcceptCoach.js
@@ -1,60 +1,56 @@
-import { useState } from 'react';
-import Modal from 'react-bootstrap/Modal'
-import { config } from '../../utils/config';
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { config } from "../../utils/config";
 
-function AcceptCoach({user_id, handleUploadSuccessChange, firstName}) {
+function AcceptCoach({ user_id, handleUploadSuccessChange, firstName }) {
+  const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleAcceptCoach = async () => {
+    try {
+      const response = await fetch(`${config.backendUrl}/get_coach_dashboard_info/coach_request/${user_id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
 
-    const handleAcceptCoach = () => {
-        try{
-            const response = fetch(`${config.backendUrl}/get_coach_dashboard_info/coach_request/${user_id}`, {
-                method: "PUT",
-                headers: {
-                // Moved data to body instead of headers
-                "Content-Type": "application/json",
-                },
-                credentials: "include", // Include credentials with the request
-            });
-            // if (!response.ok) {
-            //     throw new Error(`Failed to accept coach. Status: ${response.status}`);
-            // }
+    //   if (!response.ok) {
+    //     throw new Error(`Failed to accept coach. Status: ${response.status}`);
+    //   }
 
-            handleUploadSuccessChange();
-            handleClose();
-        } catch(err){
-            console.log(err);
-        }
+      handleUploadSuccessChange();
+      handleClose();
+    } catch (err) {
+      console.error(err);
     }
+  };
 
-    return (
-        <>
-            <button onClick={handleShow} className="btn btn-success">
-                Accept
-            </button>
+  return (
+    <>
+      <button onClick={handleShow} className="btn btn-success">
+        Accept
+      </button>
 
-            <Modal
-            show={show}
-            onHide={handleClose}
-            backdrop="static"
-            keyboard={false}
-            centered
-            >
-            <Modal.Header className='text-center' closeButton>
-                <Modal.Title className='w-100'>
-                Accept Coach
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <center>Are you sure you want accept coach "{firstName}"? <p/><b>This action cannot be undone.</b></center>
-                <button className='btn btn-success w-100 mt-2' onClick={handleAcceptCoach}>Accept</button>
-            </Modal.Body>
-            </Modal>
-        </>
-    );
+      <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} centered>
+        <Modal.Header className="text-center" closeButton>
+          <Modal.Title className="w-100">Accept Coach</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <center>
+            Are you sure you want accept coach "{firstName}"? <p />
+            <b>This action cannot be undone.</b>
+          </center>
+          <button className="btn btn-success w-100 mt-2" onClick={handleAcceptCoach}>
+            Accept
+          </button>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
 export default AcceptCoach;
--- react-app/src/components/Admin/RejectCoach.js
+++ react-app/src/components/Admin/RejectCoach.js
@@ -1,61 +1,56 @@
-import { useState } from 'react';
-import Modal from 'react-bootstrap/Modal'
-import { config } from '../../utils/config';
+import { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import { config } from "../../utils/config";
 
-function RejectCoach({user_id, handleUploadSuccessChange, firstName}) {
+function RejectCoach({ user_id, handleUploadSuccessChange, firstName }) {
+  const [show, setShow] = useState(false);
 
-    const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleRejectCoach = async () => {
+    try {
+      const response = await fetch(`${config.backendUrl}/get_coach_dashboard_info/coach_request/${user_id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        credentials: "include",
+      });
 
-    const handleRejectCoach = () => {
-        try{
-            const response = fetch(`${config.backendUrl}/get_coach_dashboard_info/coach_request/${user_id}`, {
-                method: "DELETE",
-                headers: {
-                // Moved data to body instead of headers
-                "Content-Type": "application/json",
-                },
-                credentials: "include", // Include credentials with the request
-            });
-            // if (!response.ok) {
-            //     throw new Error(`Failed to accept coach. Status: ${response.status}`);
-            // }
+    //   if (!response.ok) {
+    //     throw new Error(`Failed to reject coach. Status: ${response.status}`);
+    //   }
 
-            handleUploadSuccessChange();
-            handleClose();
-        } catch(err){
-            console.log(err);
-        }
+      handleUploadSuccessChange();
+      handleClose();
+    } catch (err) {
+      console.log(err);
     }
+  };
 
+  return (
+    <>
+      <button onClick={handleShow} className="btn btn-danger">
+        Reject
+      </button>
 
-    return (
-        <>
-            <button onClick={handleShow} className="btn btn-danger">
-                Reject
-            </button>
-
-            <Modal
-            show={show}
-            onHide={handleClose}
-            backdrop="static"
-            keyboard={false}
-            centered
-            >
-            <Modal.Header className='text-center' closeButton>
-                <Modal.Title className='w-100'>
-                Reject Coach
-                </Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <center>Are you sure you want reject coach "{firstName}"? <p/><b>This action cannot be undone.</b></center>
-                <button className='btn btn-danger w-100 mt-2' onClick={handleRejectCoach}>Reject</button>
-            </Modal.Body>
-            </Modal>
-        </>
-    );
+      <Modal show={show} onHide={handleClose} backdrop="static" keyboard={false} centered>
+        <Modal.Header className="text-center" closeButton>
+          <Modal.Title className="w-100">Reject Coach</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <center>
+            Are you sure you want reject coach "{firstName}"? <p />
+            <b>This action cannot be undone.</b>
+          </center>
+          <button className="btn btn-danger w-100 mt-2" onClick={handleRejectCoach}>
+            Reject
+          </button>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
 
 export default RejectCoach;
